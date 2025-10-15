9ddb1372db87a7b42941be4409552758ed99b1a9
wonzzu <wonzzu@naver.com>
2025-10-15 17:03:15 +0900

    feat: 직원 등록 페이지 생성

---
+++ src/router/employeeRoutes.js
@@ -0,0 +1,11 @@
+import EmployeeManegment from "@/views/employee/EmployeeManegment.vue"
+
+const employeeRoutes = [
+  {
+    path: '/employee',
+    name: 'employee',
+    component: EmployeeManegment
+  }
+]
+
+export default employeeRoutes
